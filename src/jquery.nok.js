9d9a7d99b9fd5c43999c1774474248bf9755f2ba
effone <effone@mybb.com>
2018-08-20 03:54:35 +0530

    SASS partial, cleanup

--- src/jquery.nok.js
+++ src/jquery.nok.js
@@ -1,18 +1,17 @@
-/*
- * JQuery nok : script
- * http://git.eff.one
- *
- * Copyright (c) 2018 effone
- *
- * Licensed same as jquery - MIT License
- * http://www.opensource.org/licenses/mit-license.php
- *
- * version: 1.0.0
- * Date: 2018-01-20
+/**
+ * JQuery nok : minimal stackable notification plugin
+ * 
+ * @author effone <me@eff.one>
+ * @copyright (c) 2018 effone
+ * @license MIT <http://www.opensource.org/licenses/mit-license.php>
+ * @version 1.0.1
+ * @website https://eff.one
+ * @since 2018-01-20
+ * @update 2018-08-20
  */
 
- (function($) {
-    $.nok = function(options) {
+(function ($) {
+    $.nok = function (options) {
         var defaults = {
             message: '',    // The message to display
             type: 'info',   // Type of message : info / error / success
@@ -22,9 +21,9 @@
         options = $.extend(defaults, options);
         var target = 'nok'; // Out of options as it is related to static id / class names in css file
         // Create message block only if there is message
-        if($.trim(options.message)){ // $.trim() for ie8 legacy support, can be used as options.message.trim()
+        if ($.trim(options.message)) { // $.trim() for ie8 legacy support, can be used as options.message.trim()
             // Create container if not exists
-            if($("#" + target).length == 0) {
+            if ($("#" + target).length == 0) {
                 $('body').append($('<div id="' + target + '"></div>'));
             }
             var msg = $('<div class="' + target + ' ' + target + '_' + options.type + '"><span>' + options.message + '</span></div>');
@@ -32,23 +31,23 @@
             $(msg).animate({
                 "right": "10px"
             }, "fast");
-            if(!options.sticky && $.isNumeric(options.stay)) {
-                setInterval(function() {
+            if (!options.sticky && $.isNumeric(options.stay)) {
+                setInterval(function () {
                     wipe(msg);
                 }, options.stay * 1000);
             }
         }
-        $(document).on('click', '.' + target, function() {
+        $(document).on('click', '.' + target, function () {
             wipe(this);
         });
 
         function wipe(me) {
             $(me).animate({
                 "right": "-300px"
-            }, function() {
-                $(me).slideUp('fast', function() { // Required for smooth transition of existing message blocks
+            }, function () {
+                $(me).slideUp('fast', function () { // Required for smooth transition of existing message blocks
                     $(me).remove();
-                    if(!$.trim($('#' + target).html()).length) { // Remove container if no message block exists
+                    if (!$.trim($('#' + target).html()).length) { // Remove container if no message block exists
                         $('#' + target).remove();
                     }
                 });
